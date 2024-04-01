9015085dac717b1eda2fa36bb849e6d64a2db8a2
victorsantanna <victor.esef@hotmail.com>
2024-04-01 10:15:47 -0300

    Criando Projeto 2 vue.js

---
+++ scripts.vue
@@ -0,0 +1,37 @@
+
+var todos = [
+    {
+        text: 'Aprender HTML, CSS e Javascript',
+        done: true
+    },
+    {
+        text: 'basic vue.js',
+        done: false
+    }
+];
+
+const todosApp ={
+    data(){
+        return{
+            todos: [window.todos],
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTodo: function(){
+            if(this.newTodo.text){
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+            } else{
+                alert('o texto é obrigatório')
+            }
+        }
+    }
+
+};
+
+Vue.createApp(todosApp).mount ('#app');
